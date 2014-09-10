2d5e2baa6df4081bb8b14a65c38d4ca2c9db2214
Stefan Brechbühl <email@pixelstrol.ch>
2014-09-10 14:30:14 +0200

    Only search as default widget

--- sidebar.php
+++ sidebar.php
@@ -12,21 +12,5 @@
 				<?php get_search_form(); ?>
 			</aside>
 
-			<aside id="archives" class="widget">
-				<h1 class="widget-title"><?php _e( 'Archives', 'penguin' ); ?></h1>
-				<ul>
-					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-				</ul>
-			</aside>
-
-			<aside id="meta" class="widget">
-				<h1 class="widget-title"><?php _e( 'Meta', 'penguin' ); ?></h1>
-				<ul>
-					<?php wp_register(); ?>
-					<li><?php wp_loginout(); ?></li>
-					<?php wp_meta(); ?>
-				</ul>
-			</aside>
-
 		<?php endif; // end sidebar widget area ?>
 	</div><!-- #secondary -->
