4e816bebda1609b1a2dbcfb75b8a119997b44fd0
Ulrich Pogson <grapplerulrich@gmail.com>
2014-09-11 20:19:48 +0200

    Remove http: so that the url is SSL compatable

--- functions.php
+++ functions.php
@@ -56,7 +56,7 @@ function penguin_setup() {
 
 	// Enable support for post thumbnails.
 	add_theme_support( 'post-thumbnails' );
-	
+
 	// Add new thumbnail sizes.
 	add_image_size( 'Penguin600X300', '600', '300', array( 'center', 'center' ) );
 
@@ -94,10 +94,10 @@ add_action( 'widgets_init', 'penguin_widgets_init' );
  */
 function penguin_scripts() {
 	wp_enqueue_style( 'penguin-style', get_stylesheet_uri() );
-	wp_enqueue_style( 'penguin-font', 'http://fonts.googleapis.com/css?family=Raleway:300,600' );
+	wp_enqueue_style( 'penguin-font', '//fonts.googleapis.com/css?family=Raleway:300,600' );
 
 	wp_enqueue_script( 'penguin-navigation', get_template_directory_uri() . '/js/navigation.js', array('jquery'), '20120206', true );
-	wp_enqueue_script( 'penguin-fitvids', get_template_directory_uri() . '/js/fitvids.js', array('jquery'), '20140528', true );	
+	wp_enqueue_script( 'penguin-fitvids', get_template_directory_uri() . '/js/fitvids.js', array('jquery'), '20140528', true );
 	wp_enqueue_script( 'penguin-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
