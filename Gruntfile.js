07282f32716d5580731726f15861dc46b702e7d2
Stefan Brechbühl <email@pixelstrol.ch>
2014-12-13 13:02:53 +0100

    Add Grunt files

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		sass: {
+			dist: {
+				options: {
+					sourcemap: 'none',
+					style: 'expanded'
+				},
+				files: {
+					'style.css': 'sass/style.scss'
+				}
+			}
+		},
+
+		fixindent: {
+			scripts: {
+				src: [
+					'style.css'
+				],
+				dest: 'style.css',
+				options: {
+					style: 'tab',
+					size: 1
+				}
+			}
+		},
+
+		cssmin: {
+			combine: {
+				files: {
+					'style.min.css': ['style.css']
+				}
+			}
+		},
+
+		uglify: {
+			my_target: {
+				files: {
+					'js/penguin.min.js': ['js/fluidvids.js', 'js/masonry-options.js', 'js/navigation.js', 'js/skip-link-focus-fix.js']
+				}
+			}
+		},
+
+		watch: {
+			options: {
+				livereload: true,
+			},
+			js: {
+				files: ['js/*.js'],
+				tasks: ['uglify'],
+			},
+			css: {
+				files: ['sass/*.scss'],
+				tasks: ['sass', 'fixindent', 'cssmin'],
+			},
+		}
+
+	});
+
+grunt.loadNpmTasks('grunt-contrib-sass');
+grunt.loadNpmTasks('grunt-fixindent');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-watch');
+
+grunt.registerTask('default', ['watch']);
+
+};
