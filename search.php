e6f66d83dfe04a3ab9983f248125fbd6e090eb7e
Ulrich Pogson <grapplerulrich@gmail.com>
2015-01-17 21:39:06 +0100

    Organise template parts

--- search.php
+++ search.php
@@ -22,7 +22,7 @@ get_header(); ?>
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'search' ); ?>
+				<?php get_template_part( 'template-parts/content', 'search' ); ?>
 
 			<?php endwhile; ?>
 			
@@ -33,7 +33,7 @@ get_header(); ?>
 
 		<?php else : ?>
 
-			<?php get_template_part( 'content', 'none' ); ?>
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
 		<?php endif; ?>
 
