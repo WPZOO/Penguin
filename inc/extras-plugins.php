a7bde151c7210c1056b15003d4def8fc6d14b562
Ulrich Pogson <grapplerulrich@gmail.com>
2015-01-31 21:45:36 +0100

    Fix Infinite Scroll
    - Fix loading of posts with new folder stucture
    - Initialize Masonary when loading new posts

--- inc/extras-plugins.php
+++ inc/extras-plugins.php
@@ -12,7 +12,18 @@
 function penguin_jetpack_setup() {
 	add_theme_support( 'infinite-scroll', array(
 		'container' => 'main',
+		'render'    => 'penguin_infinite_scroll_render',
 		'footer'    => 'page',
 	) );
 }
 add_action( 'after_setup_theme', 'penguin_jetpack_setup' );
+
+/**
+ * Add Infinite Scroll compatibility after content.php has been moved in template-parts folder.
+ */
+function penguin_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		get_template_part( 'template-parts/content', get_post_format() );
+	}
+}
--- js/masonry-options.js
+++ js/masonry-options.js
@@ -1,14 +1,34 @@
 /*
  * Initialization and options of Masonry
  */
-
 var container = document.querySelector('#posts-container');
-var msnry;
+var msnry = new Masonry( container, {
+	columnWidth: 400,
+	gutter: 32,
+	itemSelector: '.post',
+	transitionDuration: '1s'
+});
 // initialize Masonry after all images have loaded
 imagesLoaded( container, function() {
-	msnry = new Masonry( container, {
-		columnWidth: 400,
-		gutter: 32,
-		itemSelector: '.hentry'
-	});
+	msnry.layout();
 });
+
+// Initialize Masonry for Jetpack Infinite Scroll
+if (typeof jQuery != 'undefined') {
+	jQuery( document ).ready( function( $ ) {
+		infinite_count = 0;
+		// Triggers on infinite scroll
+		$( document.body ).on( 'post-load', function () {
+			infinite_count = infinite_count + 1;
+			var $container = $('#infinite-view-' + infinite_count);
+			// initialize Masonry after all images have loaded
+			$container.imagesLoaded( function() {
+				$container.masonry({
+					columnWidth: 400,
+					gutter: 32,
+					itemSelector: '.post'
+				});
+			});
+		});
+	});
+}
