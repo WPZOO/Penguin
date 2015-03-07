2d26be74c35a053e10d1d16217272c17c1a13cb9
Ulrich Pogson <grapplerulrich@gmail.com>
2015-03-08 00:35:04 +0100

    Add support for wpzoo/grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,72 +1,9 @@
-module.exports = function(grunt) {
-
-	grunt.initConfig({
-
-		sass: {
-			dist: {
-				options: {
-					sourcemap: 'none',
-					style: 'expanded'
-				},
-				files: {
-					'style.css': 'sass/style.scss'
-				}
-			}
-		},
-
-		fixindent: {
-			scripts: {
-				src: [
-					'style.css'
-				],
-				dest: 'style.css',
-				options: {
-					style: 'tab',
-					size: 1
-				}
-			}
-		},
-
-		cssmin: {
-			combine: {
-				files: {
-					'style.min.css': ['style.css']
-				}
-			}
-		},
-
-		uglify: {
-			my_target: {
-				files: {
-					'js/penguin.min.js': ['js/masonry-options.js', 'js/smooth-scroll.js', 'js/navigation.js', 'js/fluidvids.js', 'js/skip-link-focus-fix.js']
-				}
-			}
-		},
-
-		watch: {
-			options: {
-				livereload: true,
-			},
-			js: {
-				files: ['js/*.js'],
-				tasks: ['uglify'],
-			},
-			css: {
-				files: ['sass/**/*.scss'],
-				tasks: ['sass', 'fixindent', 'cssmin'],
-			},
+/* global require, process */
+module.exports = function( grunt ) {
+	// Load Grunt plugin configurations
+	require('load-grunt-config')(grunt, {
+		data: {
+			pkg: grunt.file.readJSON('package.json')
 		}
-
 	});
-
-grunt.loadNpmTasks('grunt-contrib-sass');
-grunt.loadNpmTasks('grunt-fixindent');
-grunt.loadNpmTasks('grunt-contrib-cssmin');
-grunt.loadNpmTasks('grunt-contrib-uglify');
-grunt.loadNpmTasks('grunt-contrib-watch');
-
-grunt.registerTask('default', ['watch']);
-grunt.registerTask('css', ['sass','fixindent','cssmin']);
-grunt.registerTask('js', ['uglify']);
-
 };
