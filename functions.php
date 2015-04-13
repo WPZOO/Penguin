abe139509acc001836a8418171955676ed48252d
Ulrich Pogson <grapplerulrich@gmail.com>
2015-04-13 18:37:46 +0200

    Simplify how minified files are loaded

--- functions.php
+++ functions.php
@@ -93,41 +93,50 @@ add_action( 'widgets_init', 'penguin_widgets_init' );
  * Enqueue scripts and styles.
  */
 function penguin_scripts() {
-	$theme = wp_get_theme();
-	$penguin = wp_get_theme( 'penguin-gold' );
+	$theme    = wp_get_theme();
+	$penguin  = wp_get_theme( 'penguin-gold' );
 	$minified = get_theme_mod( 'min-files' );
+	$suffix   = ( 1 == $minified ) ? '.min' : '';
 
-	if ( $minified == 0 && is_child_theme() ) {
-		wp_enqueue_style( 'penguin-parent-style', get_template_directory_uri() . '/style.css', false, $penguin['Version'] );
+	if ( is_child_theme() ) {
+		wp_enqueue_style( 'penguin-parent-style', get_template_directory_uri() . '/style' . $suffix . '.css', false, $penguin['Version'] );
 	}
-	if ( $minified == 1 && is_child_theme() ) {
-		wp_enqueue_style( 'penguin-parent-min-style', get_template_directory_uri() . '/style.min.css', false, $penguin['Version'] );
-	}
-	if ( $minified == 1 && !is_child_theme() ) {
-		wp_enqueue_style( 'penguin-min-style', get_template_directory_uri() . '/style.min.css', false, $penguin['Version'] );
-	}
-	else {
-		wp_enqueue_style( 'penguin-style', get_stylesheet_uri(), false, $theme['Version'] );
-	}
+
+	wp_enqueue_style( 'penguin-style', get_stylesheet_uri(), false, $theme['Version'] );
 	wp_enqueue_style( 'penguin-font', '//fonts.googleapis.com/css?family=Raleway:300,600' );
 
-	if ( $minified == 1 ) {
-		wp_enqueue_script( 'penguin-min-js', get_template_directory_uri() . '/js/penguin.min.js', array( 'masonry', 'jquery' ), $theme['Version'], true );
+	if ( is_home() || is_archive() || is_search() ) {
+		wp_enqueue_script( 'penguin-masonry-options', get_template_directory_uri() . '/js/masonry-options' . $suffix . '.js', array( 'masonry' ), '1.0', true );
 	}
-	if ( $minified == 0 && is_home() || is_archive() || is_search() ) {
-		wp_enqueue_script( 'penguin-masonry-options', get_template_directory_uri() . '/js/masonry-options.js', array( 'masonry' ), '1.0', true );
-	}
-	if ( $minified == 0 ) {
-		wp_enqueue_script( 'smooth-scroll', get_template_directory_uri() . '/js/smooth-scroll.js', array(), '5.3.3', true );
-		wp_enqueue_script( 'penguin-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-		wp_enqueue_script( 'fluidvids', get_template_directory_uri() . '/js/fluidvids.js', array(), '2.4.1', true );
-		wp_enqueue_script( 'penguin-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-	}
+
+	wp_enqueue_script( 'smooth-scroll', get_template_directory_uri() . '/js/smooth-scroll' . $suffix . '.js', array(), '5.3.3', true );
+	wp_enqueue_script( 'penguin-navigation', get_template_directory_uri() . '/js/navigation' . $suffix . '.js', array(), '20120206', true );
+	wp_enqueue_script( 'fluidvids', get_template_directory_uri() . '/js/fluidvids' . $suffix . '.js', array(), '2.4.1', true );
+	wp_enqueue_script( 'penguin-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix' . $suffix . '.js', array(), '20130115', true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'penguin_scripts' );
+
+/**
+ * Change the stylesheet_uri accordingly
+ */
+function penguin_stylesheet_uri( $stylesheet_uri, $stylesheet_dir_uri ) {
+	$minified = get_theme_mod( 'min-files' );
+	if ( is_child_theme() && 1 == $minified ) {
+		$min_stylesheet_uri = $stylesheet_dir_uri . '/style.min.css';
+		if ( file_exists( $min_stylesheet_uri ) ) {
+			$stylesheet_uri = $min_stylesheet_uri;
+		}
+	} elseif ( 1 == $minified ) {
+		$stylesheet_uri = $stylesheet_dir_uri . '/min.style.css';
+	}
+	
+	return $stylesheet_uri;
+}
+add_action( 'stylesheet_uri', 'penguin_stylesheet_uri', 10, 2 );
 
 /**
  * Make custom image size selectable from WordPress admin.
