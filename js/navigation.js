57289d374c6781742b61b3aa9abcd7cb1912d13e
Ulrich Pogson <grapplerulrich@gmail.com>
2015-05-25 19:10:49 +0200

    Fix bug with scroll to top button
    The scroll to top button was covering the mobile menu link as it was
    transparent but not hidden.
    Extended the inline docs to make it clearer

--- js/navigation.js
+++ js/navigation.js
@@ -55,27 +55,38 @@
 })();
 
 /**
- * Scroll to Top link
+ * Init smoothScroll for the scroll to top link
  */
 smoothScroll.init({
-	speed: 500, // Integer. How fast to complete the scroll in milliseconds
-	easing: 'easeInOutCubic', // Easing pattern to use
-	offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
-	updateURL: true, // Boolean. Whether or not to update the URL with the anchor hash on scroll
+	speed: 500,                     // Integer. How fast to complete the scroll in milliseconds
+	easing: 'easeInOutCubic',       // Easing pattern to use
+	offset: 0,                      // Integer. How far to offset the scrolling anchor location in pixels
+	updateURL: true,                // Boolean. Whether or not to update the URL with the anchor hash on scroll
 	callbackBefore: function () {}, // Function to run before scrolling
-	callbackAfter: function () {} // Function to run after scrolling
+	callbackAfter: function () {}   // Function to run after scrolling
 });
 
-// http://codepen.io/foleyatwork/pen/wxurt
+/**
+ * Scroll to top link
+ * Adapted from http://codepen.io/foleyatwork/pen/wxurt
+ * Uses smooth scroll for a smother experiance
+ */
 (function () {
 	var button = document.getElementById('scroll-to-top'), opacity;
 	var max = 1;
 
 	function setButtonOpacity () {
-		if (screen.height < 100 ) return;
-		opacity = window.scrollY / (screen.height + 100);
-		button.style.display = 'block';
-		button.style.opacity = opacity < max ? opacity : max;
+		if ( screen.height < 100 ) {
+			return;
+		} else {
+			opacity = window.scrollY / (screen.height + 100);
+			if ( opacity > 0 ) {
+				button.style.display = 'block';
+			} else {
+				button.style.display = 'none';
+			}
+			button.style.opacity = opacity < max ? opacity : max;
+		}
 	}
 
 	if (window.addEventListener) { // For all major browsers, except IE 8 and earlier
