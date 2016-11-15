67499088453d83a3fbd8819d2e687bffe5365561
Stefan Brechbühl <email@pixelstrol.ch>
2016-11-15 07:47:53 +0100

    Use <title> without id
    These ids cause html errors because of multiple use on the page. This works with Chrome 49 and higher, see https://css-tricks.com/accessible-svgs/#article-header-id-9

--- template-parts/meta-bottom.php
+++ template-parts/meta-bottom.php
@@ -3,9 +3,8 @@
 	$categories_list = get_the_category_list( __( ', ', 'penguin' ) );
 	if ( $categories_list && penguin_categorized_blog() ) : ?>
 	<div class="cat-links small">
-		<svg version="1.1" aria-labelledby="icon-category-title icon-category-desc" class="penguin-icon-category" role="img">
-			<title id="icon-category-title"><?php __( 'Categories', 'penguin' ) ?></title>
-			<desc id="icon-category-desc"><?php __( 'Folder icon', 'penguin' ) ?></desc>
+		<svg version="1.1" class="penguin-icon-category" role="img">
+			<title><?php _e( 'Categories icon', 'penguin' ) ?></title>
 			<use xlink:href="<?php echo get_template_directory_uri() ?>/icons.svg#penguin-icon-category"></use>
 		</svg>
 		<?php echo $categories_list; ?>
@@ -18,9 +17,8 @@
 		if ( $tags_list ) :
 	?>
 	<div class="tags-links small">
-		<svg version="1.1" aria-labelledby="icon-tag-title icon-tag-desc" class="penguin-icon-tag" role="img">
-			<title id="icon-tag-title"><?php __( 'Tags', 'penguin' ) ?></title>
-			<desc id="icon-tag-desc"><?php __( 'Tag icon', 'penguin' ) ?></desc>
+		<svg version="1.1" class="penguin-icon-tag" role="img">
+			<title><?php _e( 'Tags icon', 'penguin' ) ?></title>
 			<use xlink:href="<?php echo get_template_directory_uri() ?>/icons.svg#penguin-icon-tag"></use>
 		</svg>
 		<?php echo $tags_list; ?>
