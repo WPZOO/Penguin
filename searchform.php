9c19fcce0b0aeb89661abc35fd4ac744a0ba1cf1
Stefan Brechbühl <email@pixelstrol.ch>
2017-01-17 21:14:06 +0100

    Add missing text domain

--- searchform.php
+++ searchform.php
@@ -8,11 +8,11 @@
 
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<label>
-		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
+		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'penguin' ) ?></span>
 		<input type="search" class="search-field"
-			placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
+			placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder', 'penguin' ) ?>"
 			value="<?php echo get_search_query() ?>" name="s"
-			title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+			title="<?php echo esc_attr_x( 'Search for:', 'label', 'penguin' ) ?>" />
 	</label>
-	<input type="submit" class="search-submit screen-reader-text" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
+	<input type="submit" class="search-submit screen-reader-text" value="<?php echo esc_attr_x( 'Search', 'submit button', 'penguin' ) ?>" />
 </form>
