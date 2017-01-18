41995f7b0a423cd0d99e833a94b4f3721031692a
Stefan Brechbühl <email@pixelstrol.ch>
2017-01-18 21:33:44 +0100

    Improve searchform

--- searchform.php
+++ searchform.php
@@ -4,15 +4,16 @@
  *
  * @package Penguin Gold
  */
+
+$unique_id = esc_attr( uniqid( 'search-form-' ) );
 ?>
 
-<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-	<label>
-		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'penguin' ) ?></span>
-		<input type="search" class="search-field"
-			placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder', 'penguin' ) ?>"
-			value="<?php echo get_search_query() ?>" name="s"
-			title="<?php echo esc_attr_x( 'Search for:', 'label', 'penguin' ) ?>" />
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<label for="<?php echo $unique_id; ?>">
+		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'penguin' ); ?></span>
 	</label>
-	<input type="submit" class="search-submit screen-reader-text" value="<?php echo esc_attr_x( 'Search', 'submit button', 'penguin' ) ?>" />
+	<input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search', 'placeholder', 'penguin' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+	<button type="submit" class="search-submit screen-reader-text">
+		<?php echo _x( 'Search', 'submit button', 'penguin' ); ?>
+	</button>
 </form>
